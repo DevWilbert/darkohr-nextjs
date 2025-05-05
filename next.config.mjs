341e4e2bce094bf99f1212ba28ede6709a03a08c
Wilbert <devbert7@gmail.com>
2025-05-05 16:23:27 +0700

    Modified middleware.ts and Move middleware.ts file to src folder

--- next.config.mjs
+++ next.config.mjs
@@ -18,15 +18,6 @@ const nextConfig = {
       },
     ],
   },
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/id',
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
