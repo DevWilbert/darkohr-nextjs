8522d6fb321db4c09b9f07dbae52f03ab06dac0f
Wilbert <devbert7@gmail.com>
2025-05-06 09:17:13 +0700

    Modified next.config.mjs and utils.ts (url strapi)

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,54 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "placehold.co",
-      },
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "1337",
-      },
-      // Production - Strapi di hosting
-      {
-        protocol: 'https',
-        hostname: 'cms.example.com',
-      },
-    ],
-  },
+
+const nextConfig = () => {
+  const protocol = process.env.NEXT_PUBLIC_STRAPI_PROTOCOL || 'http'; // Default ke http jika tidak ada
+  const hostname = process.env.NEXT_PUBLIC_STRAPI_HOST || 'localhost'; // Default ke localhost
+  const port = process.env.NEXT_PUBLIC_STRAPI_PORT || '5000';  // Default ke 5000
+
+  return {
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "placehold.co",
+        },
+        {
+          protocol,
+          hostname,
+          port,
+        // Production - Strapi di hosting
+        },
+        {
+          protocol: 'https',
+          hostname: 'cms.example.com',
+        },
+      ],
+    },
+  };
 };
 
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "placehold.co",
+//       },
+//       {
+//         protocol: "http",
+//         hostname: "localhost",
+//         port: "1337",
+//       },
+//       // Production - Strapi di hosting
+//       {
+//         protocol: 'https',
+//         hostname: 'cms.example.com',
+//       },
+//     ],
+//   },
+// };
+
 export default nextConfig;
+
+
+
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -40,12 +40,15 @@ export function flattenAttributes(data: any): any {
 
 export function getStrapiURL() {
   return process.env.STRAPI_BASE_URL ?? "http://localhost:1337";
-  
+}
+
+export function getStrapiURLClient() {
+  return process.env.NEXT_PUBLIC_STRAPI_BASE_URL ?? "http://localhost:1337";
 }
 
 export function getStrapiMedia(url: string | null) {
   if (url == null) return null;
   if (url.startsWith("data:")) return url;
   if (url.startsWith("http") || url.startsWith("//")) return url;
-  return `${getStrapiURL()}${url}`;
+  return `${getStrapiURLClient()}${url}`;
 }
